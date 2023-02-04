ba821a21a7257dbfd2b53a7bb703ae7d7686bd86
SerhiiRodin <rodinserj@ukr.net>
2023-02-04 20:14:20 +0200

    add

--- js/interval.js
+++ js/interval.js
@@ -121,72 +121,141 @@ const startBtn = document.querySelector('button[data-action-start]');
 const stopBtn = document.querySelector('button[data-action-stop]');
 const clockface = document.querySelector('.js-clockface');
 
-startBtn.addEventListener('click', () => {
-  timer.start();
+// ----------------- Класс------------------------------------------------------
+class Timer {
+  constructor({ onTick }) {
+    this.intervalId = null;
+    this.isActive = false;
+    this.onTick = onTick;
+
+    this.init(); // запускаем функцию чтоб сразу отрисовало 00:00:00
+  }
+  // Функция вывод 00:00:00 при начале работы
+  init() {
+    this.onTick(this.getTimeComponents(0));
+  }
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    const startTime = Date.now();
+
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = currentTime - startTime;
+      const { hours, minutes, seconds } = this.getTimeComponents(deltaTime);
+
+      this.onTick(this.getTimeComponents(deltaTime));
+
+      console.log(`${hours}:${minutes}:${seconds}`);
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+    this.onTick(this.getTimeComponents(0));
+  }
+
+  getTimeComponents(time) {
+    const milliseconds = this.pad(parseInt((time % 1000) / 100));
+    const seconds = this.pad(parseInt((time / 1000) % 60));
+    const minutes = this.pad(parseInt((time / (1000 * 60)) % 60));
+    const hours = this.pad(parseInt((time / (1000 * 60 * 60)) % 24));
+
+    return { hours, minutes, seconds };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
+
+const timer = new Timer({
+  onTick: updateClockface,
 });
 
-stopBtn.addEventListener('click', () => {
-  timer.stop();
-});
+// Если не поставить вызов ф-ций в контексте bind(timer), то они будут ссылаться на контекст кнопки startBtn, stopBtn.
+startBtn.addEventListener('click', timer.start.bind(timer));
+stopBtn.addEventListener('click', timer.stop.bind(timer));
 
-//Функция вывода времени
 function updateClockface({ hours, minutes, seconds }) {
   clockface.textContent = `${hours}:${minutes}:${seconds}`;
 }
 
 // Вывод в формате HH:MM:SS
-const timer = {
-  //Вводим intervalId чтоб можно было очистить таймер
-  intervalId: null,
-  // Водим переменую, для того чтоб нельзя было запустить более одного таймера
-  isActive: false,
-  start() {
-    //проверка на запущенный таймер
-    if (this.isActive) {
-      return;
-    }
-    // Время запуска ф-ции
-    const startTime = Date.now();
-    // При первом запуске присваиваем true что таймер запущен
-    this.isActive = true;
+//-----------------Объект-----------------------------------------------------------
 
-    this.intervalId = setInterval(() => {
-      //Время запуска колбэк ф-ции
-      const currentTime = Date.now();
-      // console.log(`currentTime: ${currentTime}`);
+// startBtn.addEventListener('click', () => {
+//   timer.start();
+// });
+// stopBtn.addEventListener('click', () => {
+//   timer.stop();
+// });
 
-      //Разница между временем запука и текущим временем
-      const deltaTime = currentTime - startTime;
-      //деструкторизируем { hours, minutes, seconds }
-      const { hours, minutes, seconds } = getTimeComponents(deltaTime);
-      updateClockface(getTimeComponents(deltaTime));
-      console.log(`${hours}:${minutes}:${seconds}`);
-    }, 1000);
-  },
+//Функция вывода времени
+// function updateClockface({ hours, minutes, seconds }) {
+//   clockface.textContent = `${hours}:${minutes}:${seconds}`;
+// }
 
-  stop() {
-    clearInterval(this.intervalId);
-    // при остановке таймера делаем его не активным
-    this.isActive = false;
-    // для сброса времени 00:00:00
-    updateClockface(getTimeComponents(0));
-  },
-};
+// const timer = {
+//   //Вводим intervalId чтоб можно было очистить таймер
+//   intervalId: null,
+//   // Водим переменую, для того чтоб нельзя было запустить более одного таймера
+//   isActive: false,
+//   start() {
+//     //проверка на запущенный таймер
+//     if (this.isActive) {
+//       return;
+//     }
+//     // Время запуска ф-ции
+//     const startTime = Date.now();
+//     // При первом запуске присваиваем true что таймер запущен
+//     this.isActive = true;
+
+//     this.intervalId = setInterval(() => {
+//       //Время запуска колбэк ф-ции
+//       const currentTime = Date.now();
+//       // console.log(`currentTime: ${currentTime}`);
+
+//       //Разница между временем запука и текущим временем
+//       const deltaTime = currentTime - startTime;
+//       //деструкторизируем { hours, minutes, seconds }
+//       const { hours, minutes, seconds } = getTimeComponents(deltaTime);
+//       updateClockface(getTimeComponents(deltaTime));
+//       console.log(`${hours}:${minutes}:${seconds}`);
+//     }, 1000);
+//   },
+
+//   stop() {
+//     clearInterval(this.intervalId);
+//     // при остановке таймера делаем его не активным
+//     this.isActive = false;
+//     // для сброса времени 00:00:00
+//     updateClockface(getTimeComponents(0));
+//   },
+// };
+//--------------------------------------------------------------------------------
 
 //Функция берет число превр. в строку и добавляет в начало нолики, если нет значения у первых
 // 2 - х чисел 1 -> 01, 7 -> 07, 12 -> 12
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
+// function pad(value) {
+//   return String(value).padStart(2, '0');
+// }
 
-function getTimeComponents(time) {
-  const milliseconds = pad(parseInt((time % 1000) / 100));
-  const seconds = pad(parseInt((time / 1000) % 60));
-  const minutes = pad(parseInt((time / (1000 * 60)) % 60));
-  const hours = pad(parseInt((time / (1000 * 60 * 60)) % 24));
+//Функция берет число в мсек и выдает сколько в нем часов, минут, сек, мсек
+// function getTimeComponents(time) {
+//   const milliseconds = pad(parseInt((time % 1000) / 100));
+//   const seconds = pad(parseInt((time / 1000) % 60));
+//   const minutes = pad(parseInt((time / (1000 * 60)) % 60));
+//   const hours = pad(parseInt((time / (1000 * 60 * 60)) % 24));
 
-  return { hours, minutes, seconds };
-}
+//   return { hours, minutes, seconds };
+// }
 
 // timer.start();
 //-----------------Функция для преобразования миллисекунд в часы, минуты, сек, мсек------------
@@ -203,3 +272,130 @@ function getTimeComponents(time) {
 //   return hours + ':' + minutes + ':' + seconds + '.' + milliseconds;
 // }
 // console.log(msToTime(3000000));
+
+// const fff = setInterval(log, 1000);
+
+// function log() {
+// console.log(Math.random());
+// }
+
+// ----------------Промисс------------------+
+
+// const promise = new Promise((resolve, reject) => {
+//   const resultPromise = Math.random() > 0.5; //имитация результата промиса
+//   console.log(resultPromise);
+
+//   setTimeout(() => {
+//     if (resultPromise) {
+//       resolve('Промис выполнен успешно');
+//     }
+//     reject('Промис выполнен с ошибкой');
+//   }, 2000);
+// });
+
+// promise
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(result => {
+//     console.log(result);
+//   });
+
+//-----------------Цепочка----------------------
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(5);
+//   }, 2000);
+// });
+
+// promise
+//   .then(value => {
+//     console.log(value); // 5
+//     return value * 2;
+//   })
+//   .then(value => {
+//     console.log(value); // 10
+//     return value * 3;
+//   })
+//   .then(value => {
+//     console.log(value); // 30
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   })
+//   .finally(() => {
+//     console.log('Final task');
+//   });
+
+//--------------------Официант----------------------------------
+
+// const makeOrder = dish => {
+//   const DELAY = 3000;
+
+//   const promise = new Promise((resolve, reject) => {
+//     const resultPromise = Math.random() > 0.5;
+//     console.log(resultPromise);
+
+//     setTimeout(() => {
+//       if (resultPromise) {
+//         resolve('Вот ваше блюдо');
+//       }
+//       reject('Закончились продукты');
+//     }, DELAY);
+//   });
+
+//   return promise;
+// };
+
+// function onmakeOrderSuccess(result) {
+//   console.log('Success');
+//   console.log(result);
+// }
+
+// function onmakeOrderError(result) {
+//   console.log('Error');
+//   console.log(result);
+// }
+
+// makeOrder('пирожок').then(onmakeOrderSuccess).catch(onmakeOrderError);
+
+//------------------------------------------------------------------------------------
+// new Promise(resolve => resolve('success value')).then(value => console.log(value));
+// // Promise.resolve('success value').then(value => console.log(value));
+
+// new Promise((resolve, reject) => reject('error')).catch(error => console.error(error));
+// // Promise.reject('error').catch(error => console.error(error));
+//------------------------------------------------------------------------------------
+//---------------Эподром----------------------------------------
+
+const horses = ['Лошадь1', 'Лошадь2', 'Лошадь3', 'Лошадь4', 'Лошадь5'];
+
+// Функция запуска одного коня.
+// Результат промиса будат объект { horse, time }.
+
+function run(horse) {
+  return new Promise((resolve, reject) => {
+    const time = getRandomeTime(2000, 4000);
+    setTimeout(() => {
+      resolve({ horse, time });
+    }, time);
+  });
+}
+
+console.log('Заезд начался');
+
+// Для дзпуска всех лошадей создаем массив промисов
+const promises = horses.map(horse => run(horse));
+console.log(promises);
+// Метод Promise.race из коллекции промисов берет только самый быстрый.
+Promise.race(promises).then(({ horse, time }) =>
+  console.log(`%c Победила лошадь ${horse} с временем ${time}`, `color: red; font-size: 15px`)
+);
+
+// Метод Promise.all выводит резкультат исполнения всех промисов.
+Promise.all(promises).then(value => console.log(value));
+
+// Функция случайного числа от Мин до Макс
+function getRandomeTime(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
